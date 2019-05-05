41de8bd311ffd2fa8b566cca6eb7658864de7345
Slama <irwin.madet@gmail.com>
2019-05-05 23:28:45 +0200

    Add collisions, kills, objectives and all the stuff. FINISHED :)

--- lost_in_space.js
+++ lost_in_space.js
@@ -6,7 +6,18 @@ const ENEMY_SIZE = 50;
 const PLAYER_VELOCITY = 300;
 const MAX_VELOCITY = 275;
 
+const DEFAULT_X_VELOCITY = 150;
+const DEFAULT_Y_VELOCITY = 50;
+
+const PROJECTILE_WIDTH = 2;
+const PROJECTILE_HEIGHT = 10;
+
+const MAX_WAVES = 10;
+const OBJECTIVE = 3;
+
 let debugging = false;
+let killedEnemies = 0;
+let waves = 0;
 
 /**
  * Dessine toutes les entités du jeu.
@@ -15,14 +26,32 @@ let debugging = false;
 function draw(canvas) {
     let context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
-    entities.player.draw();
-    entities.enemies.forEach(e => e.draw());
-    entities.projectiles.forEach(p => p.draw());
+    if (entities.player.alive) {
+        if (killedEnemies < OBJECTIVE) {
+            entities.player.draw();
+            entities.enemies.forEach(e => e.draw());
+            entities.projectiles.forEach(p => p.draw());
+        } else {
+            context.fillStyle = 'orange';
+            context.textAlign = 'center';
+            context.font = 'normal 40px sans-serif';
+            context.fillText("Victoire !", canvas.width / 2, canvas.height / 2);
+        }
+    } else {
+        context.fillStyle = 'red';
+        context.textAlign = 'center';
+        context.font = 'normal 40px sans-serif';
+        context.fillText("Game over", canvas.width / 2, canvas.height / 2);
+    }
 
     if (debugging) {
         context.fillStyle = 'green';
+        context.font = 'normal 10px sans-serif';
+        context.textAlign = 'left';
+
         context.fillText("FPS: " + Math.round(1 / delta), 10, 20);
         context.fillText("Avg gap: " + delta + "s", 10, 30);
+        context.fillText("Kills: " + killedEnemies, 10, 150);
         context.fillText("E: " + (entities.enemies.length + entities.projectiles.length + 1), 10, canvas.height - 10);
 
         context.fillText('Up: ' + keys.ArrowUp, 10, 50);
@@ -37,8 +66,17 @@ function draw(canvas) {
  */
 function update(delta) {
     entities.player.update(delta);
-    entities.enemies.forEach(e => e.update(delta));
-    entities.projectiles.forEach(p => p.update(delta));
+    entities.enemies.forEach(e => {
+        if (e.alive) e.update(delta);
+        else {
+            killedEnemies++;
+            entities.enemies.splice(entities.enemies.indexOf(e), 1);
+        }
+    });
+    entities.projectiles.forEach(p => {
+        if (p.alive) p.update(delta);
+        else entities.projectiles.splice(entities.projectiles.indexOf(p), 1);
+    });
 }
 
 /**
@@ -50,7 +88,8 @@ function gameLoop(canvas) {
     delta = (date.getTime() - lastTime) / 1000;
     lastTime = date.getTime();
 
-    update(delta);
+    if (entities.player.alive && killedEnemies < OBJECTIVE) update(delta);
+
     draw(canvas);
 }
 
@@ -98,13 +137,29 @@ function drawPolygon(context, x, y, style, width, sides, radius, rotation = 0) {
  * @param entity Entité.
  */
 function drawHitBox(entity) {
-    drawElement(entity.context, 'green', false, 1, () => {
+    drawElement(entity.context, 'red', false, 1, () => {
         entity.context.moveTo(entity.x - entity.leftOffset, entity.y - entity.topOffset);
         entity.context.lineTo(entity.x + entity.rightOffset, entity.y - entity.topOffset);
         entity.context.lineTo(entity.x + entity.rightOffset, entity.y + entity.bottomOffset);
         entity.context.lineTo(entity.x - entity.leftOffset, entity.y + entity.bottomOffset);
         entity.context.lineTo(entity.x - entity.leftOffset, entity.y - entity.topOffset);
     });
+}
+
+/**
+ * Déclenche une vague de 30 vaisseaux ennemis.
+ */
+function spawnWave(canvas) {
+    if (waves === MAX_WAVES) return;
+    waves++;
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 10; j++) {
+            entities.enemies.push(
+                new Enemy(canvas.getContext('2d'), canvas.width * (j + 1) / 11, canvas.height * (i + 1) / 11, ENEMY_SIZE / 2)
+            );
+        }
+    }
 }
 
 class Player {
@@ -114,6 +169,136 @@ class Player {
     y;
     radius;
     rotation;
+
+    alive = true;
+
+    leftOffset = 0;
+    bottomOffset = 0;
+    rightOffset = 0;
+    topOffset = 0;
+
+    sides = 3;
+
+    constructor(context, x, y, radius, rotation = 0) {
+        this.context = context;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.rotation = rotation;
+    }
+
+    draw() {
+        drawPolygon(this.context, this.x, this.y, 'white', 2, this.sides, this.radius, this.rotation);
+        if (debugging) drawHitBox(this);
+    }
+
+    update(delta) {
+
+        entities.projectiles.forEach(p => {
+            if (this.x - this.leftOffset <= p.x + p.rightOffset && p.x + p.rightOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= p.y - p.topOffset && p.y - p.topOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                p.alive = false;
+            }
+
+            if (this.x - this.leftOffset <= p.x - p.leftOffset && p.x - p.leftOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= p.y - p.topOffset && p.y - p.topOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                p.alive = false;
+            }
+
+            if (this.x - this.leftOffset <= p.x + p.rightOffset && p.x + p.rightOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= p.y + p.bottomOffset && p.y + p.bottomOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                p.alive = false;
+            }
+
+            if (this.x - this.leftOffset <= p.x - p.leftOffset && p.x - p.leftOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= p.y + p.bottomOffset && p.y + p.bottomOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                p.alive = false;
+            }
+        });
+
+        entities.enemies.forEach(e => {
+            if (this.x - this.leftOffset <= e.x + e.rightOffset && e.x + e.rightOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= e.y - e.topOffset && e.y - e.topOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                e.alive = false;
+            }
+
+            if (this.x - this.leftOffset <= e.x - e.leftOffset && e.x - e.leftOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= e.y - e.topOffset && e.y - e.topOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                e.alive = false;
+            }
+
+            if (this.x - this.leftOffset <= e.x + e.rightOffset && e.x + e.rightOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= e.y + e.bottomOffset && e.y + e.bottomOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                e.alive = false;
+            }
+
+            if (this.x - this.leftOffset <= e.x - e.leftOffset && e.x - e.leftOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= e.y + e.bottomOffset && e.y + e.bottomOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                e.alive = false;
+            }
+        });
+
+        let width = this.context.canvas.width;
+        let height = this.context.canvas.height;
+
+        this.setOffsets();
+
+        if (keys.ArrowUp && !keys.ArrowDown && (this.y - this.topOffset) > 0) this.y -= delta * PLAYER_VELOCITY;
+        if (keys.ArrowDown && !keys.ArrowUp && (this.y + this.bottomOffset) < height) this.y += delta * PLAYER_VELOCITY;
+        if (keys.ArrowLeft && !keys.ArrowRight && (this.x - this.leftOffset) > 0) this.x -= delta * PLAYER_VELOCITY;
+        if (keys.ArrowRight && !keys.ArrowLeft && (this.x + this.rightOffset) < width) this.x += delta * PLAYER_VELOCITY;
+    }
+
+    setOffsets() {
+        this.topOffset = 0;
+        this.bottomOffset = 0;
+        this.rightOffset = 0;
+        this.leftOffset = 0;
+
+        for (let i = 0; i < this.sides; i++) {
+            let angle = i * 2 * Math.PI / this.sides + Math.PI / 2 + this.rotation;
+
+            if (this.x - Math.cos(angle) * this.radius < this.x - this.leftOffset)
+                this.leftOffset = Math.cos(angle) * this.radius;
+
+            if (this.x - Math.cos(angle) * this.radius > this.x + this.rightOffset)
+                this.rightOffset = Math.abs(Math.cos(angle)) * this.radius;
+
+            if (this.y - Math.sin(angle) * this.radius < this.y - this.topOffset)
+                this.topOffset = Math.sin(angle) * this.radius;
+
+            if (this.y - Math.sin(angle) * this.radius > this.y + this.bottomOffset)
+                this.bottomOffset = Math.abs(Math.sin(angle)) * this.radius;
+        }
+    }
+
+    shoot() {
+        entities.projectiles.push(
+            new Projectile(this.context, this.x, this.y - this.topOffset - PROJECTILE_HEIGHT, PLAYER_VELOCITY)
+        );
+    }
+
+}
+
+class Enemy {
+
+    context;
+    x;
+    y;
+    radius;
+    rotation;
+
+    xVelocity;
+    yVelocity;
+    alive = true;
 
     leftOffset = 0;
     bottomOffset = 0;
@@ -129,91 +314,44 @@ class Player {
         this.radius = radius;
         this.rotation = rotation;
 
-        for (let i = 0; i < this.sides; i++) {
-            let angle = i * 2 * Math.PI / this.sides + Math.PI / 2 + this.rotation;
-
-            if (this.x - Math.cos(angle) * this.radius < this.x - this.leftOffset)
-                this.leftOffset = Math.cos(angle) * this.radius;
-
-            if (this.x - Math.cos(angle) * this.radius > this.x + this.rightOffset)
-                this.rightOffset = Math.abs(Math.cos(angle)) * this.radius;
-
-            if (this.y - Math.sin(angle) * this.radius < this.y - this.topOffset)
-                this.topOffset = Math.sin(angle) * this.radius;
-
-            if (this.y - Math.sin(angle) * this.radius > this.y + this.bottomOffset)
-                this.bottomOffset = Math.abs(Math.sin(angle)) * this.radius;
-        }
+        this.xVelocity = this.x < this.context.canvas.width / 2 ? DEFAULT_X_VELOCITY : -DEFAULT_X_VELOCITY;
+        this.yVelocity = DEFAULT_Y_VELOCITY;
     }
 
     draw() {
-        drawPolygon(this.context, this.x, this.y, 'white', 2, this.sides, this.radius, this.rotation);
+        drawPolygon(this.context, this.x, this.y, 'green', 2, this.sides, ENEMY_SIZE / 2, this.rotation);
         if (debugging) drawHitBox(this);
     }
 
     update(delta) {
-        let width = this.context.canvas.width;
-        let height = this.context.canvas.height;
+        this.setOffsets();
 
-        if (keys.ArrowUp && !keys.ArrowDown && (this.y - this.topOffset) > 0) this.y -= delta * PLAYER_VELOCITY;
-        if (keys.ArrowDown && !keys.ArrowUp && (this.y + this.bottomOffset) < height) this.y += delta * PLAYER_VELOCITY;
-        if (keys.ArrowLeft && !keys.ArrowRight && (this.x - this.leftOffset) > 0) this.x -= delta * PLAYER_VELOCITY;
-        if (keys.ArrowRight && !keys.ArrowLeft && (this.x + this.rightOffset) < width) this.x += delta * PLAYER_VELOCITY;
-    }
+        entities.projectiles.forEach(p => {
+            if (this.x - this.leftOffset <= p.x + p.rightOffset && p.x + p.rightOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= p.y - p.topOffset && p.y - p.topOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                p.alive = false;
+            }
 
-}
+            if (this.x - this.leftOffset <= p.x - p.leftOffset && p.x - p.leftOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= p.y - p.topOffset && p.y - p.topOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                p.alive = false;
+            }
 
-class Enemy {
+            if (this.x - this.leftOffset <= p.x + p.rightOffset && p.x + p.rightOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= p.y + p.bottomOffset && p.y + p.bottomOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                p.alive = false;
+            }
 
-    context;
-    x;
-    y;
-    radius;
-    rotation;
+            if (this.x - this.leftOffset <= p.x - p.leftOffset && p.x - p.leftOffset <= this.x + this.rightOffset &&
+                this.y - this.topOffset <= p.y + p.bottomOffset && p.y + p.bottomOffset <= this.y + this.bottomOffset) {
+                this.alive = false;
+                p.alive = false;
+            }
+        });
 
-    xVelocity;
-    yVelocity;
-
-    leftOffset = 0;
-    bottomOffset = 0;
-    rightOffset = 0;
-    topOffset = 0;
-
-    sides = 5;
-
-    constructor(context, x, y, radius, rotation = 0) {
-        this.context = context;
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.rotation = rotation;
-
-        this.xVelocity = this.x < this.context.canvas.width / 2 ? -100 : 100;
-        this.yVelocity = 50;
-
-        for (let i = 0; i < this.sides; i++) {
-            let angle = i * 2 * Math.PI / this.sides + Math.PI / 2 + this.rotation;
-
-            if (this.x - Math.cos(angle) * this.radius < this.x - this.leftOffset)
-                this.leftOffset = Math.cos(angle) * this.radius;
-
-            if (this.x - Math.cos(angle) * this.radius > this.x + this.rightOffset)
-                this.rightOffset = Math.abs(Math.cos(angle)) * this.radius;
-
-            if (this.y - Math.sin(angle) * this.radius < this.y - this.topOffset)
-                this.topOffset = Math.sin(angle) * this.radius;
-
-            if (this.y - Math.sin(angle) * this.radius > this.y + this.bottomOffset)
-                this.bottomOffset = Math.abs(Math.sin(angle)) * this.radius;
-        }
-    }
-
-    draw() {
-        drawPolygon(this.context, this.x, this.y, 'white', 2, this.sides, ENEMY_SIZE / 2, this.rotation);
-        if (debugging) drawHitBox(this);
-    }
-
-    update(delta) {
         if (this.xVelocity < 0) {
             if (this.x - this.leftOffset < 0)
                 this.xVelocity *= Math.abs(this.xVelocity) < MAX_VELOCITY ? -1.1 : -1;
@@ -228,8 +366,81 @@ class Enemy {
             if (this.y + this.bottomOffset > this.context.canvas.height)
                 this.yVelocity *= Math.abs(this.yVelocity) < MAX_VELOCITY ? -1.1 : -1;
         }
+
         this.x += delta * this.xVelocity;
         this.y += delta * this.yVelocity;
+
+        this.rotation = Math.atan(this.yVelocity / this.xVelocity) + Math.PI / 2;
+        if (this.xVelocity < 0) this.rotation += Math.PI;
+    }
+
+    setOffsets() {
+        this.topOffset = 0;
+        this.bottomOffset = 0;
+        this.rightOffset = 0;
+        this.leftOffset = 0;
+
+        for (let i = 0; i < this.sides; i++) {
+            let angle = i * 2 * Math.PI / this.sides + Math.PI / 2 + this.rotation;
+
+            if (this.x - Math.cos(angle) * this.radius < this.x - this.leftOffset)
+                this.leftOffset = Math.cos(angle) * this.radius;
+
+            if (this.x - Math.cos(angle) * this.radius > this.x + this.rightOffset)
+                this.rightOffset = Math.abs(Math.cos(angle)) * this.radius;
+
+            if (this.y - Math.sin(angle) * this.radius < this.y - this.topOffset)
+                this.topOffset = Math.sin(angle) * this.radius;
+
+            if (this.y - Math.sin(angle) * this.radius > this.y + this.bottomOffset)
+                this.bottomOffset = Math.abs(Math.sin(angle)) * this.radius;
+        }
+    }
+
+}
+
+class Projectile {
+
+    context;
+    x;
+    y;
+    velocity;
+
+    traveledDistance = 0;
+    alive = true;
+
+    leftOffset = PROJECTILE_WIDTH / 2;
+    rightOffset = PROJECTILE_WIDTH / 2;
+    bottomOffset = PROJECTILE_HEIGHT / 2;
+    topOffset = PROJECTILE_HEIGHT / 2;
+
+    constructor(context, x, y, velocity) {
+        this.context = context;
+        this.x = x;
+        this.y = y;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        if (!this.alive) return;
+        drawElement(this.context, 'white', true, 1, () => {
+            this.context.moveTo(this.x - this.leftOffset, this.y - this.topOffset);
+            this.context.lineTo(this.x + this.leftOffset, this.y - this.topOffset);
+            this.context.lineTo(this.x + this.rightOffset, this.y + this.bottomOffset);
+            this.context.lineTo(this.x - this.leftOffset, this.y + this.bottomOffset);
+            this.context.lineTo(this.x - this.leftOffset, this.y - this.topOffset);
+        });
+        if (debugging) drawHitBox(this);
+    }
+
+    update(delta) {
+        if (!this.alive || this.y + this.bottomOffset <= 0 || this.traveledDistance >= this.context.canvas.height / 2) {
+            this.alive = false;
+        }
+
+        this.velocity *= .99;
+        this.y -= delta * this.velocity;
+        this.traveledDistance += delta * this.velocity;
     }
 
 }
@@ -252,10 +463,20 @@ window.onload = function () {
 
     const CANVAS = document.getElementById('game_area');
 
+    spawnWave(CANVAS);
+
     window.setInterval(() => gameLoop(CANVAS), 1000 / FRAMERATE);
+    window.setInterval(() => spawnWave(CANVAS), 5 * 1000);
     window.onkeydown = function (e) {
         if (e.key in keys) keys[e.key] = true;
-        else if (e.key.toLowerCase() === 'i') debugging = !debugging;
+        else switch (e.key.toLowerCase()) {
+            case 'i':
+                debugging = !debugging;
+                break;
+            case ' ':
+                entities.player.shoot();
+                break;
+        }
     };
     window.onkeyup = function (e) {
         if (e.key in keys) keys[e.key] = false;
@@ -263,6 +484,5 @@ window.onload = function () {
 
     entities.player = new Player(CANVAS.getContext('2d'), CANVAS.width / 2, CANVAS.height * 5 / 6, PLAYER_SIZE / 2);
 
-    entities.enemies.push(new Enemy(CANVAS.getContext('2d'), CANVAS.width / 2, CANVAS.height * 5 / 6, ENEMY_SIZE / 2));
 
 };
